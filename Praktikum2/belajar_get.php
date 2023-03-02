c6f6deb41da9d574af93a553d72cb69d46899f4e
Jnberlianaaa <114551871+Jnberlianaaa@users.noreply.github.com>
2023-03-02 15:25:45 +0700

    02/03/2023

---
+++ Praktikum2/belajar_get.php
@@ -0,0 +1,13 @@
+<?php
+$nama = $_GET['nama'];
+$jenis_kelamin = $_GET['jenis_kelamin'];
+$hoby = $_GET['Hoby'];
+$nim = $_GET['NIM'];
+$prodi = $_GET['prodi'];
+
+echo "<h2>form pendaftaran</h2><br>";
+echo "Nama : ", $nama , "<br>";
+echo "jenis kelamin : ", $jenis_kelamin , "<br>";
+echo "hoby : ", $hoby , "<br>";
+echo "nim : ", $nim , "<br>";
+echo "program studi : ", $prodi;
---
+++ Praktikum2/belajar_post.php
@@ -0,0 +1,13 @@
+<?php
+$nama = $_POST['nama'];
+$jenis_kelamin = $_POST['jenis_kelamin'];
+$hoby = $_POST['Hoby'];
+$nim = $_POST['NIM'];
+$prodi = $_POST['prodi'];
+
+echo "<h2>form pendaftaran</h2><br>";
+echo "Nama : ", $nama , "<br>";
+echo "Jenis Kelamin : ", $jenis_kelamin , "<br>";
+echo "Hoby : ", $hoby , "<br>";
+echo "NIM : ", $nim , "<br>";
+echo "Program Studi : ", $prodi;
